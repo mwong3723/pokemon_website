a98bb7aafa83ad79fcf08c1e4279d3f5465cf171
Matthew <mrw2003x@gmail.com>
2025-09-14 14:26:26 -0700

    Added more to homepage

--- Client/pokemon-website/src/components/home-page-components/top-content-section/top-content-section.jsx
+++ Client/pokemon-website/src/components/home-page-components/top-content-section/top-content-section.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import homeImage from '../../../assets/images/pkm-home-page.jpg'
 
 function TopContentSection() {
     return (
         <>
         <div className='outer-most-main-container'>
             <div className='pokeworld-container-left'>
-                <h1>PokéWorld</h1>
+                <h1>Welcome To PokéWorld</h1>
+                <h3>Discover, collect, and battle with your favorite Pokémon. 
+                    Join millions of trainers in the ultimate Pokémon experience with games, cards, 
+                    and endless adventures.</h3>
+
+                <button type='button' className='home-page-pokedex-button'>Explore PokéDex</button>
+
             </div>
             <div className='home-img-right'>
-                {/* <img></img> */}
-                <p>img</p>
+                <img src={homeImage} className='pkm-home-img'></img>
             </div>
         </div> 
        
